f4f89068b0093d73b505f0e0c0443f9088b4e1d9
Shikhar <sharwaliashikhar@gmail.com>
2026-01-15 00:11:51 +0530

    Add variable in python

---
+++ main.py
@@ -0,0 +1,49 @@
+#Strings
+
+from operator import truediv
+
+
+full_name = "Shikhar Sharwalia"
+print(f"My name is : {full_name}")
+
+email = "shikharsharwalia@outlook.com"
+print(f"Your email is : {email}")
+
+#integers
+
+age=22
+print(f"You are {age} years old My friend")
+
+
+num_of_students = 28
+print(f"total no. of student in your class are {num_of_students}")
+
+price_of_grocery = 10.99
+print(f"the price of your grocery is $ {price_of_grocery}")
+
+
+
+#boolean
+
+is_student = True
+
+if is_student :
+    print("You are a Student")
+else :
+    print("You are not a Student")
+
+
+is_sale = False
+if is_sale :
+    print("The Sale is live")
+else :
+    print("This Sale is not live")
+
+
+
+is_online = False
+
+if is_online : 
+    print("You are Live")
+else:
+    print("You are offline")
