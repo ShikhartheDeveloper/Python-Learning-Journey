82a13f6e5edfed9b8c857f5ce3e07ec510fb73c4
Shikhar <sharwaliashikhar@gmail.com>
2026-01-17 01:43:01 +0530

    Add Code to my codebase

---
+++ AreaAndVolumeOfRectange.py
@@ -0,0 +1,11 @@
+#           Aera
+length  = float(input("Enter the length of a rectangle : "))
+width = float(input("Enter the width of a rectangle : "))
+area = length * width
+print(f"The area of rectangle is {area} cm^2")
+
+#                       Volume
+
+height = float(input("Enter the height of a rectangle : "))
+volume = height * length * width
+print(f"The volume of rectangle is {volume} cm^3")
