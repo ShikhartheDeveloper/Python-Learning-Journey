ab9a7f6a8ec1cf55473c5dafd585fbfff09f043b
Shikhar <sharwaliashikhar@gmail.com>
2026-01-17 01:18:41 +0530

    Add Mad Libs Program to my CodeBase

---
+++ MadLibs.py
@@ -0,0 +1,11 @@
+adjective1 = input("Enter an adjective : ")     #am giving suspicious
+noun = input("Enter a noun")                    #am giving TonyStark
+objective2 = input("Enter an adjective : ")     #am giving berserk  
+verb = input("Enter a verb")                    #am giving screech
+objective3 = input("Enter an adjective : ")     #am giving amazed
+
+
+print(f"Today i went to a {adjective1} Zoo.")       
+print(f"In an exhibit, i saw {noun}")
+print(f"{noun} was {objective2} and {verb}ing")
+print(f"I was {objective3}")
