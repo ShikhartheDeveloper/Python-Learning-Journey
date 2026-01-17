ee82708c01848874370e85d7dff58b056d99b916
Shikhar <sharwaliashikhar@gmail.com>
2026-01-18 01:08:00 +0530

    Add Math.py to my codebase

---
+++ Math.py
@@ -0,0 +1,55 @@
+friends = 0
+
+# friends = friends + 1
+
+friends += 1
+
+print(friends)
+
+
+cars = 4
+cars -= 2
+print(cars)
+
+
+rose = 5
+rose *= 5
+print(rose)
+
+
+
+bikes = 5
+# bikes = bikes / 2
+bikes /= 2
+print(bikes)
+
+
+trucks = 4
+trucks = trucks ** 2
+print(trucks)
+
+
+apples = 10
+apples %= 3
+print(apples)
+
+
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)           #it convert 3.14 into nearest integer value, which are 3 
+print(result)
+
+
+res = pow(z , 3)            #it convert the z value as z^3 means 5^3 which are equal to 125
+print(res)
+
+#min() max()
+
+k = max(x,y,z)
+print(k)
+
+l = min(x,y,z)
+print(l)
