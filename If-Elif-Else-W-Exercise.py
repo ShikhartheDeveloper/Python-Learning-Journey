79c2e582644a85ebebd9927e1fc693d362d0dd00
Shikhar <sharwaliashikhar@gmail.com>
2026-01-19 01:08:02 +0530

    Add to my codebase

---
+++ If-Elif-Else-W-Exercise.py
@@ -0,0 +1,15 @@
+# age = int(input("Enter your age : "))
+# if age > 18:
+#     print("You are now signed up")
+# else : 
+#     print("You must be 18+ to signup")
+
+    #Exercise 1
+
+price  = float(input("Enter the price of any item : "))
+if price > 100 :
+    print("Price is too high")
+elif price > 49:
+    print("Price is good")
+else :
+    print("Price is very low")
